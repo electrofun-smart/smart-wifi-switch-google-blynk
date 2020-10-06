b4af7b934dc983813984ff5149a22f7a764b4318
electrofun-smart <67351352+electrofun-smart@users.noreply.github.com>
2020-10-06 15:39:05 -0300

    Update smart-wifi-switch-google-blynk.ino

--- smart-wifi-switch-google-blynk.ino
+++ smart-wifi-switch-google-blynk.ino
@@ -1,7 +1,7 @@
+
 #include <EEPROM.h>
 #include <ESP8266WiFi.h> //http://github.com/esp8266/Arduino
 #include <MQTTClient.h>
-//needed for library
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h> //http://github.com/tzapu/WiFiManager
@@ -15,23 +15,24 @@ Ticker ticker;
 
 #define UPDATE_INTERVAL 1000 // 1000 miliseconds = 1 seconds
 
-//define your default values here, if there are different values in config.json, they are overwritten.
+//message values on captive portal input fields
 char blynk_token[34] = "Add your BlynkToken here";
 char device_google[16] = "device id here";
+char userid[16] = "user id here";
 
 WiFiClient net;
 
 MQTTClient client;
+
 // MQTT info and Google Smart Home User Id - change the values below according to your project
 const char *thehostname = "postman.cloudmqtt.com"; // change to your mqtt broker
 const char *user = "xxxx"; // change to your mqtt user
 const char *user_password = "yyyy";// change to your mqtt password
 const char *id = "ESP01-Smart-Outlet"; //// change to your mqtt clientid
-const char *userId = "9999"; // change to your Google Smart Home user on Java backend and Firebase
+// -------------------------------------------------------------------------------
 
-#define RELAY_PIN 0         // Relay output pin GPIO 0 (ESP8266-1)
-#define RESET_INTERVAL 5000 // 5000 miliseconds = 5 seconds
-uint64_t updateTimestamp = 0;
+#define RELAY_PIN 0        // Relay output pin GPIO 0 (ESP8266-1)
+const byte keyPin = 2;     // Reset pin GPIO 2 (ESP8266-1) - keep pushed for 5 seconds to reset
 
 unsigned long keyPrevMillis = 0;
 const unsigned long keySampleIntervalMs = 25;
@@ -39,7 +40,6 @@ byte longKeyPressCountMax = 200;    // 200 * 25 = 5000 ms
 byte longKeyPressCount = 0;
 
 byte prevKeyState = HIGH;         // button is active low
-const byte keyPin = 2;            // Reset pin GPIO 2 (ESP8266-1) - keep pushed for 5 seconds to reset
 
 //flag for saving data
 bool shouldSaveConfig = false;
@@ -70,6 +70,7 @@ void setup()
     // id/name placeholder/prompt default length
     WiFiManagerParameter custom_blynk_token("blynk", "blynk token", blynk_token, 34);
     WiFiManagerParameter custom_device_google("deviceid", "device id", device_google, 16);
+    WiFiManagerParameter custom_userid_google("userid", "user id", userid, 16);
 
     //WiFiManager
     //Local intialization. Once its business is done, there is no need to keep it around
@@ -81,6 +82,7 @@ void setup()
     //add all your parameters here
     wifiManager.addParameter(&custom_blynk_token);
     wifiManager.addParameter(&custom_device_google);
+    wifiManager.addParameter(&custom_userid_google);
 
     //reset settings - for testing
     //wifiManager.resetSettings();
@@ -90,9 +92,9 @@ void setup()
 
     //fetches ssid and pass and tries to connect
     //if it does not connect it starts an access point with the specified name
-    //here  "SmartSwitch"
+    //here  "SmartSwitch-02"
     //and goes into a blocking loop awaiting configuration
-    if (!wifiManager.autoConnect("SmartSwitchElectrofun", "12345678")) // default password 12345678 change if you want
+    if (!wifiManager.autoConnect("SmartSwitch-05", "12345678"))
     {
         Serial.println("failed to connect and hit timeout");
         delay(3000);
@@ -110,6 +112,7 @@ void setup()
     //read updated parameters
     strcpy(blynk_token, custom_blynk_token.getValue());
     strcpy(device_google, custom_device_google.getValue());
+    strcpy(userid, custom_userid_google.getValue());
 
     //save the custom parameters to EEPROM
     if (shouldSaveConfig)
@@ -121,11 +124,12 @@ void setup()
     Serial.println("local ip");
     Serial.println(WiFi.localIP());
 
-    client.begin(thehostname, 16157, net);
-    client.onMessage(messageReceived);
-    connect();
-    delay(1000);
-
+    if (device_google != ""){
+        client.begin(thehostname, 16157, net);
+        client.onMessage(messageReceived);
+        connect();
+        delay(1000);
+    }
     Blynk.config(blynk_token);
 
     if (!Blynk.connect())
@@ -189,10 +193,12 @@ void loadEEPROMdata()
     EEPROM.begin(512);
     EEPROM.get(0, blynk_token);
     EEPROM.get(100, device_google);
+    EEPROM.get(150, userid);
     EEPROM.end();
     Serial.println("Recovered credentials from EEPROM:");
     Serial.println(blynk_token);
     Serial.println(device_google);
+    Serial.println(userid);
 }
 
 void saveEEPROMdata()
@@ -200,17 +206,17 @@ void saveEEPROMdata()
     EEPROM.begin(512);
     EEPROM.put(0, blynk_token);
     EEPROM.put(100, device_google);
+    EEPROM.put(150, userid);
     EEPROM.commit();
     EEPROM.end();
     Serial.println("Save credentials from EEPROM:");
     Serial.println(blynk_token);
     Serial.println(device_google);
+    Serial.println(userid);
 }
 
 void loop()
 {
-    uint64_t now = millis();
-
     if (WiFi.status() == WL_CONNECTED)
     {
         if (!client.connected())
@@ -241,13 +247,6 @@ void loop()
         }
        
         prevKeyState = currKeyState;
-    }
-
-    if ((now - updateTimestamp) > UPDATE_INTERVAL)
-    {
-       updateTimestamp = now;
-       //int relpin = digitalRead(RELAY_PIN);
-       //Blynk.virtualWrite(1, relpin); // update led status on Blynk
     }
 }
 
@@ -348,7 +347,7 @@ void sendhttp(int state)
   StaticJsonBuffer<256> jsonBuffer;
   JsonObject &res = jsonBuffer.createObject();
 
-  res["userId"] = userId;
+  res["userId"] = userid;
   res["deviceId"] = device_google;
   res["errorCode"] = "";
 
